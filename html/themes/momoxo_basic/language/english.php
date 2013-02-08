ca673170b1c8da31eb85dec010deb66ad1adfd27
hamaco <hamanaka.kazuhiro@gmail.com>
2013-02-08 12:53:11 +0900

    add momoxo_basic theme

---
+++ html/themes/momoxo_basic/language/english.php
@@ -0,0 +1,24 @@
+<?php
+//global
+define('_BASIC_EVENT_INFO', 'event info');
+define('_BASIC_WELCOME', 'welcome');
+define('_BASIC_WELCOME_2', '');
+define('_BASIC_HOME_NAME', 'home');
+define('_BASIC_LOGIN', 'login：');
+define('_BASIC_LOGIN_BUTTON', ' OK ');
+define('_BASIC_LOGIN_FORM', ' login ');
+define('_BASIC_PWORD', 'password：');
+define('_BASIC_ADMIN_PAGE', 'admin page');
+define('_BASIC_LOGOUT', 'logout');
+define('_BASIC_LOST_PASS', 'lost password');
+define('_BASIC_RESISTER', 'resister?');
+define('_BASIC_REM', 'rem');
+define('_BASIC_NORTIFICATION', 'nortification');
+define('_BASIC_USER_ACCOUNT', 'user account');
+define('_BASIC_EDIT_ACCOUNT', 'edit account');
+define('_BASIC_VIEWPMSG', 'view pm msg');
+define('_BASIC_USER_NAME', 'user name');
+define('_BASIC_NOSCRIPT', 'pleas use Javascript');
+define('_BASIC_SEARCH', 'search');
+
+?>
---
+++ html/themes/momoxo_basic/manifesto.ini.php
@@ -0,0 +1,19 @@
+<?php
+/**
+
+[Manifesto]
+Name="momoxo Basic"
+Depends=Legacy_RenderSystem,legacy
+Url="http://momoxo.org/"
+Version="0.1"
+
+[Theme]
+RenderSystem="Legacy_RenderSystem"
+Format="XOOPS2 Legacy Style"
+Author="momoxo project"
+ScreenShot="screenshot.png"
+Description="momoxo Basic Theme"
+Licence="GPL"
+
+*/
+?>
