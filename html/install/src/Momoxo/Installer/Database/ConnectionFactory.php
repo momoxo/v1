69476d19c3549ac5cdd0830dfa6239fd37bd79b4
hamaco <hamanaka.kazuhiro@gmail.com>
2013-06-08 19:20:11 +0900

    文字化け直したと思う

--- html/install/src/Momoxo/Installer/Database/ConnectionFactory.php
+++ html/install/src/Momoxo/Installer/Database/ConnectionFactory.php
@@ -62,6 +62,7 @@ class ConnectionFactory
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_AUTOCOMMIT         => true,
             PDO::ATTR_EMULATE_PREPARES   => false,
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET CHARACTER SET `utf8`',
         );
     }
 }
